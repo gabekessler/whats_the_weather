6e081c0d280bdce7da22102ba1795d6bf50b4e7d
Gabe Kessler <gabe@gabesstudio.com>
2023-03-07 23:15:59 -0500

    added validation and geocode to address

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,11 @@
 class Address < ApplicationRecord
+    # Address
+    # We really only care about the :zipcode for weather retrieval
+    has_one :weather
+
+    validates_presence_of :zipcode
+
+    # This is the 
+    geocoded_by :zipcode
+    after_validation :geocode 
 end
