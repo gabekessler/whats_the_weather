33f76edccb231a63f7c9d14cd46212f12e363fa4
Gabe Kessler <gabe@gabesstudio.com>
2023-03-09 00:50:44 -0500

    added service test

--- test/services/get_forcast_test.rb
+++ test/services/get_forcast_test.rb
@@ -3,11 +3,17 @@ require "test_helper"
 class GetForecastTest < ActiveSupport::TestCase
     setup do
         @address = addresses(:good)
-        @bad_address = addresses(:bad)
+        @bad_address = addresses(:no_geocode)
     end
 
     test "should return a hash with good params" do
         service = GetForecast.new(@address).call
         assert_equal service, {"temperature": 74, "forecast": "windy"}
     end
+
+    # TODO: test for error handling
+    # test "should return an Integer with bad params" do
+    #     service = GetForecast.new(@bad_address).call
+    #     assert_equal service, {"temperature": 74, "forecast": "windy"}
+    # end
 end
